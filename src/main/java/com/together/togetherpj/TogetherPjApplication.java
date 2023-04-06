95968dbc92cccc15e4a657c4f6e5fdcd19719dbf
Suhyeong <reosis96@gmail.com>
2023-04-06 16:07:51 +0900

    TogetherPJ

---
+++ src/main/java/com/together/togetherpj/TogetherPjApplication.java
@@ -0,0 +1,13 @@
+package com.together.togetherpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherPjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TogetherPjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/together/togetherpj/TogetherPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.together.togetherpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherPjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
