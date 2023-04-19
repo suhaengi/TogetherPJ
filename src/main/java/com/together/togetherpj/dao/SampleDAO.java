5bcc41df72a202a043ffc097003b12b42df5481a
multisula <multisula@gmail.com>
2023-04-19 13:25:59 +0900

    layout

--- src/main/java/com/together/togetherpj/dao/SampleDAO.java
+++ src/main/java/com/together/togetherpj/dao/SampleDAO.java
@@ -3,7 +3,10 @@ package com.together.togetherpj.dao;
 import com.together.togetherpj.dto.SampleDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface SampleDAO {
     void insert (SampleDTO sampleDTO);
+    List<SampleDTO> getList();
 }
