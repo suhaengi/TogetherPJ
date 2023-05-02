81f45c6659308f1d8941f5a0e510e155532f7fe2
Suhyeong <reosis96@gmail.com>
2023-05-02 15:49:11 +0900

    chat holding

--- src/main/resources/static/chat/public/js/app.js
+++ src/main/resources/static/chat/public/js/app.js
@@ -1,26 +1,49 @@
+// 소켓 객체 생성 및 서버 연결
 const socket = io("http://localhost:8085", { path: "/socket.io", transports: ["websocket"] });
 
+// HTML 문서 내의 요소들을 가져와 변수에 저장
 const welcome = document.getElementById("welcome");
-/*const form = welcome.querySelector("form");*/
+const form = welcome.querySelector("form");
 const room = document.getElementById("room");
 
-/*room.hidden = true;*/
-
+// 채팅방 요소는 화면에서 숨김처리
+room.hidden = true;
 let roomName;
 
+// 채팅방 메세지 함수
 function addMessage(message) {
     const ul = room.querySelector("ul");
     const li = document.createElement("li");
     li.innerText = message;
     ul.appendChild(li);
 }
+/*
+// 저장된 닉네임이 있는지 확인하고 있으면 그 값을 가져오기
+function loadNickname() {
+    const savedNickname = localStorage.getItem("nickname");
+    if (savedNickname) {
+        socket.send(makeMessage("nickname", savedNickname));
+    }
+}
 
+loadNickname();
+
+// 새로 입력된 닉네임을 로컬 스토리지에 저장
 function handleNickSubmit(event) {
     event.preventDefault();
     const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
-}
+    const nickname = input.value;
+    socket.send(makeMessage("nickname", nickname));
+    localStorage.setItem("nickname", nickname);
+}*/
 
+const nickNameDiv = document.getElementById("nick");
+const nickNameH3 = nickNameDiv.querySelector("h3");
+nickNameH3.textContent = nickname;
+
+
+// 메시지 입력 폼에서 입력을 받아와 서버로 새로운 메시지를 전송하는 함수
+// 소켓 메시지로 전송하고, 전송이 완료된 후 화면에 메시지를 추가함
 function handleMessageSubmit(event) {
     event.preventDefault();
     const input = room.querySelector("#msg input");
@@ -31,6 +54,20 @@ function handleMessageSubmit(event) {
     input.value = "";
 }
 
+function showWelcome() {
+    welcome.hidden = false;
+    room.hidden = true;
+    const nickForm = welcome.querySelector("#nick");
+    nickForm.addEventListener("submit", handleNickSubmit);
+    const roomForm = welcome.querySelector("#room");
+    roomForm.addEventListener("submit", handleRoomSubmit);
+    loadNickname(); // 추가
+}
+
+
+// 채팅방 화면을 보여주는 함수
+// 기존에는 숨겨져 있던 채팅방 요소를 보이게 함
+// 메시지 입력 폼의 이벤트 핸들러를 추가함
 function showRoom() {
     welcome.hidden = true;
     room.hidden = false;
@@ -40,6 +77,9 @@ function showRoom() {
     msgForm.addEventListener("submit", handleMessageSubmit);
 }
 
+
+// 새로운 채팅방에 입장하기 위한 함수
+// 채팅방 이름을 입력받아 서버로 전송하고, 채팅방 화면을 보여주는 함수를 콜백으로 전달함
 function handleRoomSubmit(event) {
     event.preventDefault();
     const input = form.querySelector("input");
@@ -48,22 +88,21 @@ function handleRoomSubmit(event) {
     input.value = "";
 }
 
-/*form.addEventListener("submit", handleRoomSubmit);
+form.addEventListener("submit", handleRoomSubmit);
 
 socket.on("welcome", (user, newCount) => {
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName} (${newCount})`;
-    addMessage(`${user} arrived!`);
-});*/
+    addMessage(`${user}님이 입장하셨습니다.`);
+});
 
 socket.on("bye", (left, newCount) => {
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName} (${newCount})`;
-    addMessage(`${left} left ㅠㅠ`);
+    addMessage(`${left}님이 채팅방을 나가셨습니다.`);
 });
 
 socket.on("new_message", addMessage);
-socket.on("submit", handleNickSubmit);
 
 socket.on("room_change", (rooms) => {
     const roomList = welcome.querySelector("ul");
--- src/main/resources/static/chat/server.js
+++ src/main/resources/static/chat/server.js
@@ -3,21 +3,41 @@ import { Server } from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 
+/*const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: '158.247.211.246', // your host name
+    user: 'wuser', // your database user
+    password: 'dbtoU!12', // your database password
+    database: 'together' // your database name
+});
+
+// Connect to the database
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+    console.log('Connected to database!');
+});*/
+
+// express 초기화
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/views"));
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
-app.get("/*", (_, res) => res.redirect("/"));
+app.get("/", (_, res) => res.sendFile("/views/home.html"));
+app.get("/*", (_, res) => res.redirect("/chat"));
 
+// socket.io 지원을 위해 http 모듈에서 제공하는 메서드로 서버를 초기화
 const httpServer = http.createServer(app);
+
+// 웹 소켓 서버를 초기화
 const wsServer = new Server(httpServer, {
     cors: {
         origin: ["https://admin.socket.io"],
         credentials: true,
     },
-    path: "/socket.io"
 });
 
 instrument(wsServer, {
@@ -44,7 +64,9 @@ function countRoom(roomName) {
 }
 
 wsServer.on("connection", (socket) => {
+    // 기본 닉네임 "익명"
     socket["nickname"] = "Anon";
+    //이벤트 발생 시 콘솔에 출력
     socket.onAny((event) => {
         console.log(`Socket Event: ${event}`);
     });
@@ -69,5 +91,5 @@ wsServer.on("connection", (socket) => {
     socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
-const handleListen = () => console.log(`Listening on http://localhost:8085/chat`);
-httpServer.listen(8083, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:8085`);
+httpServer.listen(8085, handleListen);
