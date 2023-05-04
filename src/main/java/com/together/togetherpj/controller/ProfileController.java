87225a461d9c55d84f67848407cf76f6aa24d475
suz <ddaall1118@gmail.com>
2023-05-04 17:17:12 +0900

    프로필이미지등록

--- src/main/java/com/together/togetherpj/controller/ProfileController.java
+++ src/main/java/com/together/togetherpj/controller/ProfileController.java
@@ -1,5 +1,6 @@
 package com.together.togetherpj.controller;
 
+import com.together.togetherpj.domain.Member;
 import com.together.togetherpj.dto.EditForm;
 import com.together.togetherpj.dto.ProfileDto;
 import com.together.togetherpj.service.ProfileService;
@@ -7,7 +8,10 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,7 +22,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 @RequestMapping("/member")
@@ -29,7 +35,7 @@ public class ProfileController {
 
     @PreAuthorize("isAuthenticated()")  //로그인한 사용자만 조회할 수 있도록
     @GetMapping("/mypage")
-    public String myPage(Model model, Authentication authentication){
+    public String myPage(Model model, Authentication authentication) throws IOException{
         String email = authentication.getName();
         ProfileDto profileDto = profileService.readOne(email);
 
@@ -68,5 +74,16 @@ public class ProfileController {
         profileService.saveImg(authentication,imgFile);
 
         return "/member/mypage";
+    }
+
+    @GetMapping("/image")
+    public ResponseEntity<?> getProfileImg (Authentication authentication) throws IOException {
+        String email = authentication.getName();
+        ProfileDto profileDto = profileService.readOne(email);
+
+        InputStream inputStream = new FileInputStream(profileDto.getProfileImgPath());
+        byte[] imageByteArray = IOUtils.toByteArray(inputStream);
+        inputStream.close();
+        return new ResponseEntity<>(imageByteArray, HttpStatus.OK);
     }
 }
