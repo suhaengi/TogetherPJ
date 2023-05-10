8ef184bc23b59f9cb125b2ee024148b5d040924a
Suhyeong <reosis96@gmail.com>
2023-05-10 12:58:15 +0900

    mainpage 수정 중

--- src/main/java/com/together/togetherpj/service/RecruitService.java
+++ src/main/java/com/together/togetherpj/service/RecruitService.java
@@ -5,13 +5,8 @@ import com.together.togetherpj.domain.Applying;
 import com.together.togetherpj.domain.Member;
 import com.together.togetherpj.domain.Recruit;
 import com.together.togetherpj.domain.id.ApplyingId;
-<<<<<<<<< Temporary merge branch 1
 import com.together.togetherpj.dto.RecruitWriteFormDto;
 import com.together.togetherpj.dto.ViewForm;
-=========
-import com.together.togetherpj.domain.id.QApplyingId;
-import com.together.togetherpj.dto.RecruitWriteFormDto;
->>>>>>>>> Temporary merge branch 2
 import com.together.togetherpj.repository.ApplyingRepository;
 import com.together.togetherpj.repository.MemberRepository;
 import com.together.togetherpj.repository.RecruitRepository;
@@ -20,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +28,6 @@ public class RecruitService {
   private final RecruitRepository recruitRepository;
   private final MemberRepository memberRepository;
   private final ApplyingRepository applyingRepository;
-<<<<<<<<< Temporary merge branch 1
 
   public ViewForm readOne(Long bno) throws IOException {
     Recruit recruit = recruitRepository.findById(bno).orElseThrow();
@@ -55,19 +50,14 @@ public class RecruitService {
             .build();
     return viewForm;
   }
-=========
->>>>>>>>> Temporary merge branch 2
 
-  public List<Recruit> findAll(){
-    return recruitRepository.findAll();
-  }
-
+  @Transactional
   public void save(RecruitWriteFormDto writeFormDto, String userEmail){
     Member writer = memberRepository.findByEmail(userEmail)
-        .orElseThrow(IllegalStateException::new);
+            .orElseThrow(IllegalStateException::new);
+
     Recruit recruit = createRecruit(writeFormDto, writer);
     Applying applying = createWriterApplying(writer, recruit);
-<<<<<<<<< Temporary merge branch 1
 
     recruitRepository.save(recruit);
     applyingRepository.save(applying);
@@ -80,53 +70,32 @@ public class RecruitService {
             .applier(writer)
             .recruit(recruit)
             .build();
-=========
-    recruitRepository.save(recruit);
-    applyingRepository.save(applying);
-
-
-
->>>>>>>>> Temporary merge branch 2
   }
 
   private Recruit createRecruit(RecruitWriteFormDto writeFormDto, Member writer){
     return Recruit.builder()
-        .title(writeFormDto.getTitle())
-        .city(writeFormDto.getCity())
-        .content(writeFormDto.getContent())
-        .perNum(Long.parseLong(writeFormDto.getPerNum()))
-        .startdate(LocalDate.parse(writeFormDto.getStartdate()))
-        .enddate(LocalDate.parse(writeFormDto.getEnddate()))
-        .state(State.RECRUITING)
-        .recruitWriter(writer)
-        .build();
+            .title(writeFormDto.getTitle())
+            .city(writeFormDto.getCity())
+            .content(writeFormDto.getContent())
+            .perNum(writeFormDto.getPerNum())
+            .startdate(LocalDate.parse(writeFormDto.getStartdate()))
+            .enddate(LocalDate.parse(writeFormDto.getEnddate()))
+            .state(State.RECRUITING)
+            .recruitWriter(writer)
+            .build();
   }
 
-<<<<<<<<< Temporary merge branch 1
   public void Applying(String email, Long bno){
     Member applier = memberRepository.findByEmail(email).orElseThrow();
     Recruit recruit = recruitRepository.findById(bno).orElseThrow();
 
-     Applying applying = Applying.builder()
-             .id(new ApplyingId())
-             .isOk(false)
-             .applier(applier)
-             .recruit(recruit)
-             .build();
-     applyingRepository.save(applying);
-=========
-
-  private Applying createWriterApplying(Member writer, Recruit recruit) {
-    Applying applying=Applying.builder()
+    Applying applying = Applying.builder()
             .id(new ApplyingId())
-            .isOk(true)
-            .applier(writer)
+            .isOk(false)
+            .applier(applier)
             .recruit(recruit)
             .build();
-
-
-    return applying;
->>>>>>>>> Temporary merge branch 2
+    applyingRepository.save(applying);
   }
 
 /*  public List<Recruit> findAll(){
@@ -136,8 +105,5 @@ public class RecruitService {
   public List<Recruit> getLatestRecruits() {
     return recruitRepository.findTop16ByOrderByModDateDesc();
   }
-
-
-
 
 }
