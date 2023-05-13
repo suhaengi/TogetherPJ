abe1b50c88d165c050c047d761591abe7618df0a
multisula <multisula@gmail.com>
2023-05-13 16:36:57 +0900

    search security

--- src/main/java/com/together/togetherpj/config/SecurityConfig.java
+++ src/main/java/com/together/togetherpj/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     http.authorizeHttpRequests()
         .mvcMatchers("/css/**", "/assets/**", "/forms/**", "/images/**").permitAll()  // static
         .mvcMatchers("/", "/member/login", "/member/register",
-                "/recruit/view", "/board/recruitBoard/**").permitAll()  // permit all templates
+                "/recruit/view", "/board/recruitBoard/**", "/board/search").permitAll()  // permit all templates
         .mvcMatchers("/member/**", "/recruit/write-form").hasRole("MEMBER")
         .mvcMatchers("/admin").hasRole("ADMIN")  // permit admin templates
         .anyRequest().authenticated();
